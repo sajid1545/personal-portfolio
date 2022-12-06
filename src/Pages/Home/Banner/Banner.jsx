ec6e797b73538b1fb28d6dd023d5436047985e7b
sajid9962 <clashking1545@gmail.com>
2022-12-07 05:02:27 +0600

    Navbar added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import './banner.css'
-
-
+import './banner.css';
+import MovingText from 'react-moving-text';
 
 const Banner = () => {
-
-  
-
 	return (
-        <div className='flex h-screen justify-center items-center flex-col  '>
-			<h1 className='font-extrabold text-8xl mb-10'>Sajjad Abdullah</h1>
-			<h3 className='text-5xl font-extrabold'>Web Developer</h3>
-			<button className="glow-on-hover mt-5 px-20 py-4 font-extrabold text-xl" type="button">Resume</button>
+		<div className="flex h-screen justify-center items-center flex-col  ">
+			<MovingText
+				type="shadow"
+				duration="1500ms"
+				delay="0s"
+				direction="normal"
+				timing="ease"
+				iteration="infinite"
+				fillMode="forwards">
+				{/* Content... */}
+				<h1 className="font-extrabold text-5xl lg:text-8xl mb-10">Sajjad Abdullah</h1>
+			</MovingText>
+			<h3 className="text-3xl lg:text-5xl font-extrabold">Web Developer</h3>
+			<a target='_blank' href="https://drive.google.com/file/d/12EhVHS0PJwue7XtpgIMbl3XxmjkIs9dG/view?usp=share_link">
+				<button className="glow-on-hover mt-10 px-14 py-4 font-extrabold text-xl" type="button">
+					See My Resume
+				</button>
+			</a>
 		</div>
 	);
 };
