f4983676066e81c2e63b3b542a997604b2f2eb0b
sajid1545 <clashking1545@gmail.com>
2023-10-05 23:38:56 +0600

    updated resume link in banner

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import './banner.css';
 import { BsDownload } from 'react-icons/bs';
+import './banner.css';
 
 const Banner = () => {
+	const handleDownload = () => {
+		fetch('/src/assets/Resume/Sajjad Abdullah MERN Stack Developer.pdf').then((response) => {
+			response.blob().then((blob) => {
+				// Creating new object of PDF file
+				const fileURL = window.URL.createObjectURL(blob);
+				// Setting various property values
+				let alink = document.createElement('a');
+				alink.href = fileURL;
+				alink.download = 'Sajjad Abdullah MERN Stack Developer.pdf';
+				alink.click();
+			});
+		});
+	};
+
 	return (
 		<div className="flex h-screen justify-center items-center flex-col  mb-10  ">
 			<h1 className="my-name font-extrabold text-5xl lg:text-8xl mb-10 ">Sajjad Abdullah</h1>
@@ -10,7 +24,7 @@ const Banner = () => {
 			<div className="flex items-center justify-center gap-5">
 				<a
 					target="_blank"
-					href="https://drive.google.com/file/d/12EhVHS0PJwue7XtpgIMbl3XxmjkIs9dG/view?usp=share_link">
+					href="https://drive.google.com/file/d/1fEhoSoyCvkzLfE24dzsBhJVvrI9Rh7tT/view?usp=sharing">
 					<button
 						className="glow-on-hover bg-[#191328] mt-10 px-14 py-4 font-extrabold text-xl"
 						type="button">
@@ -19,10 +33,12 @@ const Banner = () => {
 				</a>
 
 				<a
-					href="/src/assets/Resume/Sajjad Abdullah Web Developer.pdf"
-					download="Sajjad-Abdullah Portfolio">
+				// href="/src/assets/Resume/Sajjad Abdullah Web Developer.pdf"
+				// download="Sajjad-Abdullah Portfolio"
+				>
 					<button
 						type="button"
+						onClick={handleDownload}
 						className="glow-on-hover bg-[#191328] mt-10 px-4 py-4 font-extrabold text-xl flex">
 						<BsDownload className="h-6 w-6" />
 					</button>
