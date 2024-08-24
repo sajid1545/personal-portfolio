a3e8dfc76eefe38f3af388e0e9b0b4d73f56b3dd
Sajjad Abdullah <clashking1545@gmail.com>
2024-08-25 01:30:07 +0600

    update handle download

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -5,14 +5,12 @@ import "./banner.css";
 
 const Banner = () => {
 	const handleDownload = () => {
-		fetch("/src/assets/Resume/fullStack.pdf").then((response) => {
+		fetch("/fullStack.pdf").then((response) => {
 			response.blob().then((blob) => {
-				// Creating new object of PDF file
 				const fileURL = window.URL.createObjectURL(blob);
-				// Setting various property values
 				const alink = document.createElement("a");
 				alink.href = fileURL;
-				alink.download = "Sajjad Abdullah MERN Stack Developer.pdf";
+				alink.download = "Sajjad Abdullah Full Stack Developer.pdf";
 				alink.click();
 			});
 		});
@@ -22,9 +20,15 @@ const Banner = () => {
 		<div className="flex h-screen justify-center items-center flex-col mb-10">
 			<motion.h1
 				className="my-name font-extrabold text-5xl lg:text-8xl mb-10"
-				initial={{ opacity: 0, y: -50 }}
+				initial={{ opacity: 0, y: -100 }}
 				animate={{ opacity: 1, y: 0 }}
-				transition={{ duration: 1 }}
+				transition={{
+					duration: 1,
+					ease: "easeOut",
+					type: "spring",
+					stiffness: 100,
+				}}
+				whileHover={{ textShadow: "0px 0px 8px rgba(255, 255, 255, 0.8)" }}
 			>
 				Sajjad Abdullah
 			</motion.h1>
@@ -32,7 +36,14 @@ const Banner = () => {
 				className="text-3xl lg:text-5xl font-extrabold"
 				initial={{ opacity: 0, y: -50 }}
 				animate={{ opacity: 1, y: 0 }}
-				transition={{ duration: 1, delay: 0.5 }}
+				transition={{
+					duration: 1,
+					delay: 0.5,
+					ease: "easeOut",
+					type: "spring",
+					stiffness: 100,
+				}}
+				whileHover={{ textShadow: "0px 0px 8px rgba(255, 255, 255, 0.8)" }}
 			>
 				Web Developer
 			</motion.h3>
@@ -48,11 +59,10 @@ const Banner = () => {
 					href="https://drive.google.com/file/d/1fEhoSoyCvkzLfE24dzsBhJVvrI9Rh7tT/view?usp=sharing"
 				>
 					<motion.button
-						className="glow-on-hover bg-[#191328] mt-10 px-14 py-4 font-extrabold text-xl"
+						className="glow-on-hover bg-gradient-to-r from-purple-600 to-blue-600 mt-10 px-14 py-4 font-extrabold text-xl text-white rounded-lg shadow-lg"
 						type="button"
 						whileHover={{
-							scale: 1.1,
-							boxShadow: "0px 0px 8px rgba(255, 255, 255, 0.8)",
+							boxShadow: "0px 0px 16px rgba(255, 255, 255, 0.9)",
 						}}
 						transition={{ duration: 0.3 }}
 					>
@@ -63,14 +73,14 @@ const Banner = () => {
 				<motion.button
 					type="button"
 					onClick={handleDownload}
-					className="glow-on-hover bg-[#191328] mt-10 px-4 py-4 font-extrabold text-xl flex"
+					className="glow-on-hover bg-gradient-to-r from-pink-500 to-red-500 mt-10 px-8 py-4 font-extrabold text-xl text-white flex items-center rounded-lg shadow-lg"
 					whileHover={{
-						scale: 1.1,
-						boxShadow: "0px 0px 8px rgba(255, 255, 255, 0.8)",
+						boxShadow: "0px 0px 16px rgba(255, 255, 255, 0.9)",
 					}}
 					transition={{ duration: 0.3 }}
 				>
-					<BsDownload className="h-7 w-7" />
+					<BsDownload className="h-7 w-7 mr-2" />
+					Download Resume
 				</motion.button>
 			</motion.div>
 		</div>
